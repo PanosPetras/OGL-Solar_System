96d2bbd435707026fd0f20c62ae2ad3489726de7
Panagiotis Petrakopoulos <npetrakopoulos2003@gmail.com>
2024-01-24 12:19:53 +0200

    Delete comments from texture.

--- GL_Texture_2Ds.cpp
+++ GL_Texture_2Ds.cpp
@@ -13,34 +13,23 @@ GL_Texture_2Ds::GL_Texture_2Ds(
 	stbi_set_flip_vertically_on_load(true);
 	unsigned char* bytes = stbi_load(image.c_str(), &widthImg, &heightImg, &numColCh, 0);
 
-	// Generates an OpenGL texture object
 	glGenTextures(1, &id);
-	// Assigns the texture to a Texture Unit
+
 	glActiveTexture(GL_TEXTURE0 + slot);
 	unit = slot;
 	glBindTexture(GL_TEXTURE_2D, id);
 
-	// Configures the type of algorithm that is used to make the image smaller or bigger
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-	// Configures the way the texture repeats (if it does at all)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-	// Extra lines in case you choose to use GL_CLAMP_TO_BORDER
-	// float flatColor[] = {1.0f, 1.0f, 1.0f, 1.0f};
-	// glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, flatColor);
-
-	// Assigns the image to the OpenGL Texture object
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, format, pixelType, bytes);
-	// Generates MipMaps
 	glGenerateMipmap(GL_TEXTURE_2D);
 
-	// Deletes the image data as it is already in the OpenGL Texture object
 	stbi_image_free(bytes);
 
-	// Unbinds the OpenGL Texture object so that it can't accidentally be modified
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
@@ -49,11 +38,8 @@ void GL_Texture_2Ds::texUnit(
 	const char* uniform,
 	GLuint unit
 ) {
-	// Gets the location of the uniform
 	GLuint texUni = glGetUniformLocation(shader.id, uniform);
-	// Shader needs to be activated before changing the value of a uniform
 	shader.Activate();
-	// Sets the value of the uniform
 	glUniform1i(texUni, unit);
 }
 
