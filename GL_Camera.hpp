faae9e30280c3038094474b1d292a0097baae423
Panagiotis Petrakopoulos <npetrakopoulos2003@gmail.com>
2024-01-24 10:18:16 +0200

    Remove Comments from camera.

--- GL_Camera.hpp
+++ GL_Camera.hpp
@@ -13,47 +13,39 @@
 
 class GL_Camera {
 protected:
-	// Stores the main vectors of the camera
 	glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, -1.0f);
 	glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
 	glm::mat4 cameraMatrix = glm::mat4(1.0f);
 
-	// Prevents the camera from jumping around when first clicking left click
 	bool firstClick = true;
 
-	// Stores the width and height of the window
 	int width;
 	int height;
 
 	int x, y;
 	float orbitRadius;
 
-	// Adjust the speed of the camera and it's sensitivity when looking around
 	float speed = 0.1f;
 	float sensitivity = 100.0f;
 
 public:
 	glm::vec3 Position;
 
-	// Camera constructor to set up initial values
 	GL_Camera(
 		int width, 
 		int height, 
 		glm::vec3 position
 	);
 
-	// Updates the camera matrix to the Vertex Shader
 	void updateMatrix(
 		float FOVdeg, 
 		float nearPlane, 
 		float farPlane
 	);
-	// Exports the camera matrix to a shader
 	void Matrix(
 		GL_Shader& shader, 
 		const char* uniform
 	);
-	// Handles camera inputs
 	void handleKeyboardInput(
 		GLFWwindow* window
 	);
