4feeaf294a811ee798ed72e0b5ff4a7b276bca3a
Panagiotis Petrakopoulos <npetrakopoulos2003@gmail.com>
2024-01-24 10:27:24 +0200

    Create file for GL_Vertex.

--- GL_VBO.hpp
+++ GL_VBO.hpp
@@ -4,13 +4,7 @@
 #include <glm/glm.hpp>
 #include <glad/glad.h>
 #include <vector>
-
-struct GL_Vertex {
-	glm::vec3 position;
-	glm::vec3 normal;
-	glm::vec3 color;
-	glm::vec2 texUV;
-};
+#include "GL_Vertex.hpp"
 
 class GL_VBO {
 protected:
---
+++ GL_Vertex.hpp
@@ -0,0 +1,11 @@
+#ifndef gl_vertex_hpp
+#define gl_vertex_hpp
+
+struct GL_Vertex {
+	glm::vec3 position;
+	glm::vec3 normal;
+	glm::vec3 color;
+	glm::vec2 texUV;
+};
+
+#endif
